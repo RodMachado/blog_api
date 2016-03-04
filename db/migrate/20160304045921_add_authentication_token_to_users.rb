426ad9b93c7160c94b420fd50abb410878012833
Rodrigo Machado <rodmac98@gmail.com>
2016-03-04 00:08:48 -0500

    Adding authentication token generator

---
+++ db/migrate/20160304045921_add_authentication_token_to_users.rb
@@ -0,0 +1,8 @@
+class AddAuthenticationTokenToUsers < ActiveRecord::Migration
+
+  def change
+    add_column :users, :auth_token, :string, default: ''
+    add_index :users, :auth_token, unique: true
+  end
+
+end
