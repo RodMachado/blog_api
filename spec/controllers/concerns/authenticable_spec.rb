e38373e412d4b40b529f825a28b0f017516faf7e
Rodrigo Machado <rodmac98@gmail.com>
2016-03-04 00:52:09 -0500

    Adds auth token method

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -7,6 +7,9 @@ class Authentication
   def request
   end
 
+  def response
+  end
+
 end
 
 describe Authenticable, type: :controller do
@@ -14,6 +17,14 @@ describe Authenticable, type: :controller do
 
   let(:request) do
     double(:request, headers: { 'Authorization' => user.auth_token })
+  end
+
+  let(:response) do
+    double(
+      :response,
+      response_code: 401,
+      body: { 'errors' => 'Not authenticated' }.to_json
+    )
   end
 
   subject { Authentication.new }
@@ -27,5 +38,18 @@ describe Authenticable, type: :controller do
     it 'returns the user from the authorization header' do
       expect(subject.current_user.auth_token).to eql(user.auth_token)
     end
+  end
+
+  describe '#authenticate_with_token' do
+    before do
+      allow(subject).to receive(:current_user).and_return(nil)
+      allow(subject).to receive(:response).and_return(response)
+    end
+
+    it 'render a json error message' do
+      expect(json_response[:errors]).to eql('Not authenticated')
+    end
+
+    it { should respond_with 401 }
   end
 end
