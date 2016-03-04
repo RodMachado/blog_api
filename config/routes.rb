54628c2e144967066acaae1e7a39c65656577b58
Rodrigo Machado <rodmac98@gmail.com>
2016-03-03 22:56:02 -0500

    User create action

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :posts
-      resources :users, only: [:show]
+      resources :users, only: [:show, :create]
     end
   end
 end
